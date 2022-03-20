a9b445cf378562237520f0171ad9a4afe6b03b73
Wilhelm Zeuschner <37621765+wilhelmzeuschner@users.noreply.github.com>
2022-03-20 15:51:14 +0100

    soft pwm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1335,7 +1335,7 @@
  * Turn on with the command 'M111 S32'.
  * NOTE: Requires a lot of PROGMEM!
  */
-//#define DEBUG_LEVELING_FEATURE
+#define DEBUG_LEVELING_FEATURE		//DO NOT DISABLE ON VYPER -> HOMING WON'T WORK
 
 #if ANY(MESH_BED_LEVELING, AUTO_BED_LEVELING_BILINEAR, AUTO_BED_LEVELING_UBL)
   // Gradually reduce leveling correction until a set height is reached,
